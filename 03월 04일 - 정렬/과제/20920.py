cd048b39e88e18670085b2a9e08d56b87bbd652b
Jiyoung Park <78730403+ZERO-black@users.noreply.github.com>
2022-03-02 17:29:13 +0900

    Delete 20920.py
    영단어 암기는 어려워 -> 맵, 셋 주차 과제로 뺐습니다.

--- 03월 04일 - 정렬/과제/20920.py
+++
@@ -1,7 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-n, k = map(int, input().split())
-# 기본 input() 함수와 달리, readline은 줄 끝에 개행문자('\n')까지 입력됩니다.
-word = [input().rstrip() for _ in range(n)]
-print(word)
