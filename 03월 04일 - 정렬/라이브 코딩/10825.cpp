702b90f159bc5504d130a57b031fb35d531de159
suabae <bsa0322@ewhain.net>
2022-03-03 16:57:18 +0900

    [정렬] 03월 03일 (라이브 코딩)

---
+++ 03월 04일 - 정렬/라이브 코딩/10825.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct student { //학생들의 정보를 나타내는 구조체
+    string name;
+    int kor, eng, math;
+};
+
+//비교 함수(bad)
+//(튜터용) 비효율적인거 먼저 보여주고 최적화한 코드 보여주려구요!
+bool cmp(const student &a, const student &b) {
+    if (a.kor == b.kor) { //국어 점수가 같으면 영어 점수가 증가하는 순서로
+        if (a.eng == b.eng) { //국어 점수와 영어 점수가 같으면 수학 점수가 감소하는 순서로
+            if (a.math == b.math) { //모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로
+                return a.name < b.name;
+            }
+            return a.math > b.math;
+        }
+        return a.eng < b.eng;
+    }
+    return a.kor > b.kor; //국어 점수가 감소하는 순서로
+}
+
+//비교 함수(good)
+bool cmpAdv(const student &a, const student &b) {
+    if (a.kor != b.kor) //1. 국어 점수가 다르다면 감소하는 순서
+        return a.kor > b.kor;
+    if (a.eng != b.eng) //2. (국어 점수는 같고) 영어 점수가 다르다면 증가하는 순서
+        return a.eng < b.eng;
+    if (a.math != b.math) //3. (국어, 영어 같고) 수학 점수가 다르다면 감소하는 순서
+        return a.math > b.math;
+    return a.name < b.name; //4. (국어, 영어, 수학 모두 같고) 이름이 사전 순으로 증가하는 순서
+}
+
+int main() {
+    int n;
+    //입력
+    cin >> n;
+    vector<student> arr(n);
+    for (int i = 0; i < n; i++) { //이름, 국어, 영어, 수학 점수
+        cin >> arr[i].name >> arr[i].kor >> arr[i].eng >> arr[i].math;
+    }
+    //연산
+    sort(arr.begin(), arr.end(), cmpAdv);
+    //출력
+    for (int i = 0; i < n; i++) {
+        cout << arr[i].name << '\n';
+    }
+    return 0;
+}
---
+++ 03월 04일 - 정렬/라이브 코딩/2751.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> arr, sorted; //정렬해야하는 배열, 중간중간 정렬한 배열
+
+//left ~ right 사이의 원소 상태 출력
+void printArr(int left, int right, string tag) {
+    cout << tag << " array: ";
+    for (int i = left; i <= right; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+}
+
+//합병 정렬(conquer & merge)
+void merge(int left, int mid, int right) {
+    //왼쪽 배열 시작(p1), 오른쪽 배열 시작(p2), 정렬한 배열 위치값(index)
+    int p1 = left, p2 = mid + 1, index = left;
+
+    while (p1 <= mid && p2 <= right) { //왼쪽 오른쪽 배열 모두 검사
+        if (arr[p1] < arr[p2]) { //왼쪽 배열의 현재 값이 오른쪽 배열의 값보다 작다면
+            sorted[index++] = arr[p1++]; //왼쪽 배열의 값 정렬하고, 인덱스 증가
+        } else { //왼쪽 배열의 현재 값보다 오른쪽 배열의 값이 작거나 같다면
+            sorted[index++] = arr[p2++]; //오른쪽 배열의 값 정렬하고, 인덱스 증가
+        }
+    }
+    while (p1 <= mid) { //왼쪽 배열에 탐색하지 않은 원소가 있다면
+        sorted[index++] = arr[p1++]; //정렬
+    }
+    while (p2 <= right) { //오른쪽 배열에 탐색하지 않은 원소가 있다면
+        sorted[index++] = arr[p2++]; //정렬
+    }
+    //원래 배열에 다시 복사 -> 이래야 다음 conquer & merge 과정에서 올바른 검사 가능
+    for (int i = left; i <= right; i++) {
+        arr[i] = sorted[i];
+    }
+}
+
+//합병 정렬(divide)
+void mergeSort(int n, int left, int right) {
+    if (left < right) { //원소 하나가 될 때까지 나눔
+        int mid = (left + right) / 2; //정확히 반으로 나눈다
+        mergeSort(n, left, mid); //왼쪽 배열
+        mergeSort(n, mid + 1, right); //오른쪽 배열
+
+        //정렬 전 배열 출력
+        printArr(left, mid, "left");
+        printArr(mid + 1, right, "right");
+
+        //정렬해서 합치기
+        merge(left, mid, right);
+
+        //정렬 후 배열 출력
+        printArr(left, right, "Merged");
+        cout << "=================\n";
+    }
+}
+
+int main() {
+    int n;
+
+    //입력
+    cin >> n;
+    arr.assign(n, 0);
+    sorted.assign(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    //연산
+    mergeSort(n, 0, n - 1);
+    //출력
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << '\n';
+    }
+    return 0;
+}
