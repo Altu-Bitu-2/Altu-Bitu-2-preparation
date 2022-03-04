da8b15ac2c3836968b440d1a5e517a5b02c02fb3
jiyoung <jiyoung_06@ewhain.net>
2022-03-04 15:31:38 +0900

    [정렬] 03월 04일 (과제 코드.py) - Update
    주석 추가, 함수 이름 스네이크 표기법으로 변경했습니다.

--- 03월 04일 - 정렬/과제/1758.py
+++ 03월 04일 - 정렬/과제/1758.py
@@ -1,6 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+# 이 문제는 최대한 많은 팁을 받아야 합니다.
+# 따라서 못 받는 팁을 최소화해야 하므로 내림차순으로 정렬합니다.
+# (오름차순으로 정렬할 경우, 순서가 밀리면서 계속 팁을 못받는 경우가 생길 수 있어요)
+
 n = int(input())
 tip_arr = [int(input()) for _ in range(n)]
 tip_arr.sort(reverse=True)  # 내림차순으로 정렬
