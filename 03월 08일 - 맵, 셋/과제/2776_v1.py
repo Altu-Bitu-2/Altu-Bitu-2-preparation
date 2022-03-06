5676d7d3642ee1f0f625e6f7a482635d59c6a4c1
jiyoung <jiyoung_06@ewhain.net>
2022-03-07 03:04:55 +0900

    [맵, 셋] 03월 07일 (과제 1개 cpp, py)
    2776_v1.py

---
+++ 03월 08일 - 맵, 셋/과제/2776_v1.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+# ver1) 셋을 이용한 풀이입니다.
+
+def solution():
+    # 입력
+    n = int(input())
+    note1 = set(map(int, input().split()))
+    m = int(input())
+    note2 = list(map(int, input().split()))
+
+    # note2에 있는 숫자를 하나씩 꺼내 note1에 있는지 비교합니다.
+    for num in note2:
+        if num in note1:
+            print(1)
+        else:
+            print(0)
+    return
+
+t = int(input())
+
+for _ in range(t):
+    solution()
