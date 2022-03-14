3d1399f273cb7875cabd2741d5fce6aa6260c89b
jiyoung <jiyoung_06@ewhain.net>
2022-03-15 01:06:12 +0900

    [정수론] 03월 15일 (과제.py)

---
+++ 03월 15일 - 정수론/과제/10610.py
@@ -0,0 +1,40 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+"""
+[30]
+30의 배수 = 10의 배수 && 3의 배수
+1. 10의 배수 -> 입력된 수에 0이 포함되어 있는지 확인
+2. 3의 배수 -> 모든 자리수의 합이 3의 배수인지 확인
+
+30의 배수임이 확인 되었으면,
+가장 큰 수를 만들기 위해 9부터 0까지 역순으로 나열한다.
+"""
+
+def find_number(n):
+    digits = Counter(n)
+
+    # 0이 존재하지 않으면, return -1
+    if '0' not in digits:
+        return -1
+    
+    answer = ""
+    total = 0   # 3의 배수인지 확인하기 위해 모든 자리수를 더함
+
+    # 9부터 0까지
+    for i in range(9, -1, -1):
+        if str(i) in digits:
+            total += i * digits[str(i)]     # 숫자를 개수만큼 곱해서 total에 더해줌
+            answer += str(i) * digits[str(i)]
+
+    # 3의 배수임을 확인
+    if total % 3 == 0:
+        return answer
+    else:
+        return -1
+
+# 입력
+n = input().rstrip()
+# 연산 + 출력
+print(find_number(n))
