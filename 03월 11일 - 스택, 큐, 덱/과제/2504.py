048033dd6f09df50ac248bde60c69b9d7785bece
jiyoung <jiyoung_06@ewhain.net>
2022-03-18 00:55:32 +0900

    [스택, 큐, 덱] 03월 18일 (update)

--- 03월 11일 - 스택, 큐, 덱/과제/2504.py
+++ 03월 11일 - 스택, 큐, 덱/과제/2504.py
@@ -1,6 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+"""
+ "분배법칙"을 활용!
+ ex. ([]([])): 2 x (3 + 2 x 3) == 2 x 3 + 2 x 2 x 3
+ =>                               (   [ ]     (   [   ]   )   )
+ =>                   임시변수값:  2   6 2     4  12   4   2   1
+ =>                        정답:      +6           +12        = 18
+
+ 따라서, 우선 여는 괄호가 나오면 괄호의 값을 곱해줌
+ 닫는 괄호는, 이전 문자가 여는 괄호일 경우 지금까지 곱한 값을 더해줌 (값이 중복으로 더해지는 것을 방지하기 위해 이전 문자가 여는 괄호인지 꼭 체크!)
+            한 괄호에 대한 연산이 끝났으므로 (곱하기 연산) 다시 괄호 값을 나눠줘서 연산 진행
+"""
+
 def calc(text):
     bracket = dict()    # 괄호 쌍 저장
     bracket[')'] = '('
@@ -30,10 +42,13 @@ def calc(text):
             stack.pop()
         prev = i
 
+    # 올바른 괄호라면
     if len(stack) == 0:
         return answer
     
     return 0
 
+# 입력
 s = input().rstrip()
+# 연산 + 출력
 print(calc(s))
