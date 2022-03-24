d580f9a3d939a24b65f96bb5241691ab92c1a0a8
jiyoung <jiyoung_06@ewhain.net>
2022-03-25 05:02:35 +0900

    [백트래킹] 03월 25일 (필수.py + 힌트)

---
+++ 03월 25일 - 백트래킹/과제/1205.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+"""
+[등수 구하기]
+
+1. n = 0일 때, 고려
+2. 등수는 p보다 작지만, 랭킹 리스트에 들어가지 못하는 경우 고려
+
+.find(value): value가 있는 첫번째 인덱스를 리턴, 없으면 에러 발생
+입력된 점수를 기존 리스트에 넣고 인덱스 구하기 -> 해당 점수의 첫번째 인덱스 리턴
+입력된 점수 -1를 기존 리스트에 넣고 인덱스 구하기 -> 해당 점수의 마지막 인덱스 리턴
+
+마지막 인덱스로 구한 등수가 p를 넘지 않으면, 첫번째 인덱스로 구한 등수가 정답
+"""
+
+n, new_score, p = map(int, input().split())
+
+if n == 0:
+    answer = 1
+else:
+    scores = list(map(int, input().split()))
+    scores.append(new_score)
+    scores.sort(reverse=True)
+
+    first_idx = scores.index(new_score)
+    scores[first_idx] -= 1
+    scores.sort(reverse=True)
+    second_idx = scores.index(new_score - 1)
+
+    if second_idx + 1 <= p:
+        answer = first_idx + 1
+    else:
+        answer = -1
+
+print(answer)
