d1713321f2c59e2c3882f9333a692d7e9aeecb78
jiyoung <jiyoung_06@ewhain.net>
2022-03-26 23:56:48 +0900

    [백트래킹] 03월 26일 (과제.py)

--- 03월 25일 - 백트래킹/과제/1205.py
+++ 03월 25일 - 백트래킹/과제/1205.py
@@ -14,20 +14,26 @@ input = sys.stdin.readline
 마지막 인덱스로 구한 등수가 p를 넘지 않으면, 첫번째 인덱스로 구한 등수가 정답
 """
 
+# 입력
 n, new_score, p = map(int, input().split())
 
 if n == 0:
     answer = 1
 else:
+    # 입력
     scores = list(map(int, input().split()))
+    
+    # 해당 점수의 가장 상위 등수 구하기
     scores.append(new_score)
     scores.sort(reverse=True)
-
     first_idx = scores.index(new_score)
+
+    # 해당 점수의 가장 하위 등수 구하기
     scores[first_idx] -= 1
     scores.sort(reverse=True)
     second_idx = scores.index(new_score - 1)
 
+    # 이미 스코어 보드가 다 찬 경우
     if second_idx + 1 <= p:
         answer = first_idx + 1
     else:
