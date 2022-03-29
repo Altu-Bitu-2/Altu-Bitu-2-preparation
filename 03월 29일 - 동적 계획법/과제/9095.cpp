0f371bbc6170a64dfc071f18abd2cf243d8673e0
suabae <bsa0322@ewhain.net>
2022-03-29 16:32:49 +0900

    [동적 계획법] 03월 29일 (선택 5개 CPP) - Update

--- 03월 29일 - 동적 계획법/과제/9095.cpp
+++ 03월 29일 - 동적 계획법/과제/9095.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+//top-down 방식 dp
+vector<int> dp_top;
+int numberOfCasesTop(int n) {
+    if (n < 0) {
+        return 0;
+    }
+    if (n == 0) {
+        return dp_top[0] = 1;
+    }
+    if (dp_top[n]) {
+        return dp_top[n];
+    }
+    for (int i = 1; i <= 3; i++) {
+        dp_top[n] += numberOfCasesTop(n - i);
+    }
+    return dp_top[n];
+}
+
+//bottom-up 방식 dp
 int numberOfCases(int n) {
     vector<int> dp(n + 1, 0);
 
@@ -15,6 +34,12 @@ int numberOfCases(int n) {
 }
 
 /**
+ * [top-down 접근]
+ * n부터 시작해서 -1, -2, -3한 수의 경우의 수 먼저 구함
+ * 0미만일 경우 합으로 나타낼 수 없는 경우이므로 0 리턴
+ * 이미 dp[n] 값이 있을 경우, 바로 리턴
+ *
+ * [bottom-up 접근]
  * 각 수를 인덱스로 써서 정수 n까지 1, 2, 3의 합으로 나타내는 방법의 수를 저장하자!
  *
  * 우선 3까지 1, 2, 3의 합으로 나타내는 방법의 수 초기화 함
@@ -34,7 +59,11 @@ int main() {
         cin >> n;
 
         //연산 & 출력
-        cout << numberOfCases(n) << '\n';
+        //bottom-up 방식
+        //cout << numberOfCases(n) << '\n';
+        //top-down 방식
+        dp_top.assign(n + 1, 0);
+        cout << numberOfCasesTop(n) << '\n';
     }
     return 0;
 }
