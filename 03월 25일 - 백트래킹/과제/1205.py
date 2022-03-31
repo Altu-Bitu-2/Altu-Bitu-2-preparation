0e0cba13381ff9de096504acc270af0fe811be03
jiyoung <jiyoung_06@ewhain.net>
2022-03-31 18:27:59 +0900

    [백트래킹] 03월 31일 (update 1205.py)

--- 03월 25일 - 백트래킹/과제/1205.py
+++ 03월 25일 - 백트래킹/과제/1205.py
@@ -9,7 +9,10 @@ input = sys.stdin.readline
 
 .find(value): value가 있는 첫번째 인덱스를 리턴, 없으면 에러 발생
 입력된 점수를 기존 리스트에 넣고 인덱스 구하기 -> 해당 점수의 첫번째 인덱스 리턴
-입력된 점수 -1를 기존 리스트에 넣고 인덱스 구하기 -> 해당 점수의 마지막 인덱스 리턴
+.count(value): 리스트에서 value의 수를 세어 리턴, 없으면 에러 발생
+전체 점수 중 동점자의 수 구하기 -> 첫번째 등수(인덱스 + 1) + 동점자 수 - 1 
+                                = 첫번째 인덱스 + 동점자 수 
+                                = 해당 점수의 마지막 등수
 
 마지막 인덱스로 구한 등수가 p를 넘지 않으면, 첫번째 인덱스로 구한 등수가 정답
 """
@@ -28,15 +31,12 @@ else:
     scores.sort(reverse=True)
     first_idx = scores.index(new_score)
 
-    # 해당 점수의 가장 하위 등수 구하기
-    scores[first_idx] -= 1
-    scores.sort(reverse=True)
-    second_idx = scores.index(new_score - 1)
+    # 동점자가 몇 명 있는지 구하기
+    same_score = scores.find(new_score)
 
-    # 이미 스코어 보드가 다 찬 경우
-    if second_idx + 1 <= p:
+    if first_idx + same_score <= p:
         answer = first_idx + 1
-    else:
+    else: # 이미 스코어 보드가 다 찬 경우
         answer = -1
 
 print(answer)
