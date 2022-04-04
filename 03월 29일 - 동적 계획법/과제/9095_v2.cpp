4ee5afd9610ae0400e8c33224ec86c63c8b2e131
suabae <bsa0322@ewhain.net>
2022-04-04 21:35:52 +0900

    [동적 계획법] 04월 04일 (선택 5개 CPP) - Update

--- 03월 29일 - 동적 계획법/과제/9095_v2.cpp
+++ 03월 29일 - 동적 계획법/과제/9095_v2.cpp
@@ -7,20 +7,20 @@ const int MAX = 10;
 vector<int> dp(MAX + 1);
 
 //top-down 방식 dp 채우기
-int numberOfAllCases(int a) {
-    if (a < 0) {
+int numberOfCasesTop(int n) {
+    if (n < 0) {
         return 0;
     }
-    if (a == 0) {
+    if (n == 0) {
         return 1;
     }
-    if (dp[a]) {
-        return dp[a];
+    if (dp[n]) {
+        return dp[n];
     }
     for (int i = 1; i <= 3; i++) {
-        dp[a] += numberOfAllCases(a - i);
+        dp[n] += numberOfCasesTop(n - i);
     }
-    return dp[a];
+    return dp[n];
 }
 
 /**
@@ -28,23 +28,18 @@ int numberOfAllCases(int a) {
  * n부터 시작해서 -1, -2, -3한 수의 경우의 수 먼저 구함
  * 0미만일 경우 합으로 나타낼 수 없는 경우이므로 0 리턴
  * 이미 dp[n] 값이 있을 경우, 바로 리턴
- *
- * 테스트케이스로 입력이 들어오고, 입력 범위가 11로 작기 때문에 미리 dp 배열 채우고 시작하는 것이 더 효율적
  */
 
 int main() {
     int t, n;
-
-    //미리 dp 채우기
-    numberOfAllCases(MAX);
 
     //입력
     cin >> t;
     while (t--) {
         cin >> n;
 
-        //출력
-        cout << dp[n] << '\n';
+        //연산 & 출력
+        cout << numberOfCasesTop(n) << '\n';
     }
     return 0;
 }
