3d1ec3d3576a20d950062a7c588b59e21852ad84
suabae <bsa0322@ewhain.net>
2022-04-10 18:44:06 +0900

    [투 포인터] 04월 10일 (라이브 코딩)

---
+++ 04월 12일 - 투 포인터/라이브 코딩/21921.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef pair<int, int> ci;
+
+//슬라이딩 윈도우로 최대 방문자 수(first)를 구하고, 그 기간의 수(second)를 리턴
+ci maxVisitor(int n, int x, vector<int> &visitor) {
+    //윈도우 초기화
+    int section_visitor = 0;
+    for (int i = 0; i < x; i++) { //첫날부터 x일 동안의 방문자 수
+        section_visitor += visitor[i];
+    }
+
+    int ans = section_visitor; //최대 방문자 수
+    int cnt = 1; //최대 방문자 수의 기간의 수
+    for (int i = x; i < n; i++) { //슬라이딩
+        section_visitor -= visitor[i - x]; //이번 윈도우에서 제외되는 값
+        section_visitor += visitor[i]; //이번 윈도우에서 추가되는 값
+        if (section_visitor > ans) { //최대 방문자 수가 갱신된다면
+            ans = section_visitor;
+            cnt = 1;
+        } else if (section_visitor == ans) { //최대 방문자 수와 같다면
+            cnt++;
+        }
+    }
+    return {ans, cnt};
+}
+
+int main() {
+    int n, x;
+
+    //입력
+    cin >> n >> x;
+    vector<int> visitor(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> visitor[i];
+    }
+
+    //연산
+    ci ans = maxVisitor(n, x, visitor);
+
+    //출력
+    if (!ans.first) { //최대 방문자 수가 0명 이라면
+        cout << "SAD\n";
+        return 0;
+    }
+    cout << ans.first << '\n' << ans.second;
+    return 0;
+}
