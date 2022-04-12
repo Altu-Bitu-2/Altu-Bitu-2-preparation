28d7f5a8d010d3f9dcab739068cfc0e09432ffb3
suabae <bsa0322@ewhain.net>
2022-04-12 16:38:35 +0900

    [투 포인터] 04월 12일 (라이브 코딩) - Update

--- 04월 12일 - 투 포인터/라이브 코딩/10025.cpp
+++ 04월 12일 - 투 포인터/라이브 코딩/10025.cpp
@@ -6,18 +6,18 @@ const int SIZE = 1e6;
 
 //슬라이딩 윈도우로 얼음들의 최대핪 구함
 int maxIce(int k, int end_point, vector<int> &position) {
-    k = k * 2 + 1; //윈도우 영역
+    int section = k * 2 + 1; //윈도우 영역
     int section_ice = 0;
     //윈도우 초기화
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < section; i++) {
         if (i > end_point) { //윈도우 영역이 양동이가 존재하는 끝 점보다 클 수도 있음
             break; //종료
         }
         section_ice += position[i];
     }
     int ans = section_ice; //얼음 최대핪
-    for (int i = k; i <= end_point; i++) {
-        section_ice -= position[i - k]; //이번 윈도우에서 제외되는 값
+    for (int i = section; i <= end_point; i++) {
+        section_ice -= position[i - section]; //이번 윈도우에서 제외되는 값
         section_ice += position[i]; //이번 윈도우에서 추가되는 값
         ans = max(ans, section_ice);
     }
