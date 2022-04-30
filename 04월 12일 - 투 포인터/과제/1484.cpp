26dbfdc6bd0b4ffdf10f4dbe0744ee91f978b43f
suabae <bsa0322@ewhain.net>
2022-04-30 23:44:12 +0900

    [투 포인터] 04월 30일 (선택.cpp) - Update

--- 04월 12일 - 투 포인터/과제/1484.cpp
+++ 04월 12일 - 투 포인터/과제/1484.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 vector<int> possibleWeight(int left, int right, int g) {
     vector<int> ans;
-    while (left <= right) {
+    while (left < right) {
         int diff = right * right - left * left;
 
-        if (right - left == 1 && diff > g) {
-            break;
-        }
         if (diff <= g) {
             if (diff == g) {
                 ans.push_back(right);
@@ -33,7 +30,9 @@ vector<int> possibleWeight(int left, int right, int g) {
  * 대신, left ~ right를 모두 고려하는 것이 아니라 left, right 포인터 값 자체만 고려
  *
  * !주의! 몸무게의 범위가 딱히 정해져 있지 않으므로, 종료 조건을 잘 세우는 것이 중요!
- *       -> 가장 인접한 몸무게 제곱근의 차이가 g를 넘으면 더 이상 탐색 불가
+ *       -> 두 몸무게가 같아지는 순간 종료!
+ *       -> left가 right와 같은 값을 가진다면, 직전 상황은 두 몸무게가 1차이 나는 상황
+ *       -> 이 때 몸무게 차이가 g 이상이었다는 것은 이후로 나올 수 있는 차이는 무조건 g이상이 된다. (제곱수의 간격은 수가 커질수록 늘어나기 때문)
  */
 
 int main() {
@@ -43,7 +42,7 @@ int main() {
     cin >> g;
 
     //연산
-    vector<int> ans = possibleWeight(1, 1, g);
+    vector<int> ans = possibleWeight(1, 2, g);
 
     //출력
     if (!ans.size()) {
