19a1094c78eafc4cdd58b905929ba60eeb16d103
suabae <bsa0322@ewhain.net>
2022-05-01 01:01:15 +0900

    [dfs&bfs] 05월 01일 (필수.cpp) - Update

--- 04월 15일 - DFS & BFS/과제/13422.cpp
+++ 04월 15일 - DFS & BFS/과제/13422.cpp
@@ -9,12 +9,11 @@ int steal(int n, int m, int k, vector<int> &house) {
     for (int i = 0; i < m; i++) {
         money += house[i];
     }
+    int ans = money < k; //훔쳐야 하는 집 초기화
 
     if (n == m) { //훔칠 수 있는 경우 딱 1개인 경우
-        return money < k;
+        return ans;
     }
-
-    int ans = money < k;
     for (int i = m; i < n + m - 1; i++) {
         money -= house[i - m];
         money += house[i % n];
--- 04월 15일 - DFS & BFS/과제/16234.cpp
+++ 04월 15일 - DFS & BFS/과제/16234.cpp
@@ -5,29 +5,28 @@
 using namespace std;
 typedef pair<int, int> ci;
 
-//상, 하, 좌, 우
-int dr[4] = {-1, 1, 0, 0};
-int dc[4] = {0, 0, -1, 1};
-vector<vector<int>> board; //각 나라 인구
+vector<vector<int>> board, visited;
+queue<ci> countries;
+
 
 //각 연합의 인구 수만큼 인구 이동하는 함수
-bool peopleMove(int n, int population, vector<vector<int>> &visited) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (visited[i][j] == 1) {
-                visited[i][j] = -1;
-                board[i][j] = population;
-            }
-        }
+void movePeople(int population, vector<ci> &union_cord) {
+    for (int i = 0; i < union_cord.size(); i++) {
+        int row = union_cord[i].first, col = union_cord[i].second;
+        board[row][col] = population;
+        countries.push({row, col}); //인구 이동이 있는 나라는 다음에도 인구 이동 가능성 있음
     }
-    return true; //인구 이동 했음을 나타냄
 }
 
-ci bfs(int n, int l, int r, int cr, int cc, vector<vector<int>> &visited) {
+bool bfs(int n, int l, int r, int cr, int cc, int day) {
+    //상, 하, 좌, 우
+    int dr[4] = {-1, 1, 0, 0};
+    int dc[4] = {0, 0, -1, 1};
+
+    vector<ci> union_cord; //연합 저장할 벡터
+    union_cord.push_back({cr, cc});
     queue<ci> q;
     q.push({cr, cc});
-    visited[cr][cc] = 1;
-
     int sum = board[cr][cc], cnt = 1;
     while (!q.empty()) {
         cr = q.front().first;
@@ -37,62 +36,59 @@ ci bfs(int n, int l, int r, int cr, int cc, vector<vector<int>> &visited) {
         for (int i = 0; i < 4; i++) {
             int nr = cr + dr[i];
             int nc = cc + dc[i];
-            if (nr < 0 || nr >= n || nc < 0 || nc >= n || visited[nr][nc]) {
+            if (nr < 0 || nr >= n || nc < 0 || nc >= n || visited[nr][nc] == day) {
                 continue;
             }
             int diff = abs(board[nr][nc] - board[cr][cc]);
             if (diff >= l && diff <= r) { //국경선 열림
                 q.push({nr, nc});
-                visited[nr][nc] = 1;
+                visited[nr][nc] = day;
+                union_cord.push_back({nr, nc}); //연합에 추가
                 sum += board[nr][nc];
                 cnt++;
             }
         }
     }
-    if (cnt == 1) { //국경선이 열린 나라가 없다면
-        visited[cr][cc] = -1;
-        return {-1, -1};
+    if (cnt > 1) { //연합이 생겼다면
+        movePeople(sum / cnt, union_cord); //인구 이동
+        return true;
     }
-    return {sum, cnt};
+    return false;
 }
 
 int simulation(int n, int l, int r) {
-    int day = 0;
+    int day = 0; //방문 체크로도 사용
     while (true) {
-        vector<vector<int>> visited(n, vector<int>(n, 0));
+        day++;
         bool is_move = false; //인구 이동 했는지 체크
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j]) { //이미 탐색한 나라라면
-                    continue;
-                }
-                ci info = bfs(n, l, r, i, j, visited);
-                if (info.first != -1) {
-                    is_move = peopleMove(n, info.first / info.second, visited);
-                }
+        int size = countries.size(); //이번 시간에 탐색할 수 있는 나라의 수
+        while (size--) {
+            int row = countries.front().first;
+            int col = countries.front().second;
+            countries.pop();
+            if (visited[row][col] == day) { //이미 탐색한 나라라면
+                continue;
+            }
+            visited[row][col] = day;
+            if (bfs(n, l, r, row, col, day)) {
+                is_move = true;
             }
         }
         if (!is_move) {
-            return day;
+            return day - 1;
         }
-        day++;
     }
 }
 
 /**
  * [인구 이동]
  *
- * 모든 나라에 대해서 처음 탐색하는 나라면 bfs 탐색 시작
- *   - bfs 탐색을 통해 국경선 오픈할 수 있는 나라 찾고 연합으로 묶기 -> 연합의 인구수와 연합을 이루고 있는 칸의 개수 리턴
- *   - 해당 연합의 인구 수 바꾸기 (인구 이동)
- *
- * 국경선 오픈할 수 있는 나라가 한 개도 없으면 -> 인구 이동 못함 -> break
- *
- * visited: 탐색 여부 저장 & 연합 구분
- *    0: 탐색 안함
- *    1: 탐색 함 & 연합
- *   -1: 이미 인구 이동이 끝난 연합
- *   국경선 오픈하는 나라 없을 때, 연합이 아니므로 1을 다시 -1로 바꿔줘야 함을 주의
+ * 0. 인구이동이 일어날 수 있는 나라(처음에는 모든 나라)의 좌표를 좌표 큐에 저장
+ * 1. bfs 탐색을 통해 연합을 확인하고, 연합에 포함된 나라의 인구이동을 진행한다.
+ * 2. 인구 이동이 있었던 나라는 다음 날에도 인구이동이 시작될 수 있으므로 좌표 큐에 다시 넣어준다.
+ *    -> 직전 이동이 있었던 나라에 대해서만 bfs 탐색 진행
+ *    - 인구 이동이 일어나지 않은 두 나라 사이에서는 다음 날에도 인구이동이 일어날 수 없음
+ * 3. 인구이동이 전혀 일어나지 않을 때까지 반복
  */
 
 int main() {
@@ -101,9 +97,11 @@ int main() {
     //입력
     cin >> n >> l >> r;
     board.assign(n, vector<int>(n, 0));
+    visited.assign(n, vector<int>(n, 0));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> board[i][j];
+            countries.push({i, j});
         }
     }
 
