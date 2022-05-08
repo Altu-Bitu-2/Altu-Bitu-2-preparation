631a1dc7dd61d5fb034eefeccffb07e5a1db71a5
suabae <bsa0322@ewhain.net>
2022-05-09 03:35:55 +0900

    [최단 경로] 05월 09일 (선택.cpp) - Update

--- 05월 03일 - 최단 경로/과제/1865.cpp
+++ 05월 03일 - 최단 경로/과제/1865.cpp
@@ -4,36 +4,32 @@
 
 using namespace std;
 typedef tuple<int, int, int> tp;
-typedef long long ll;
 const int INF = 5e6; //최대 n-1개의 간선을 지나게 됨 -> n * (가중치 최대값)
 
-vector<ll> bellmanFord(int start, int n, int m, vector<tp> &edges) {
-    //최솟값 갱신하는 과정에서 언더플로우 일어날 수 있음
-    vector<ll> dist(n + 1, INF);
+vector<int> bellmanFord(int start, int n, vector<tp> &edges) {
+    vector<int> dist(n + 1, INF);
 
-    //시작 정점 초기화
     dist[start] = 0;
     for (int i = 1; i <= n; i++) {
-        bool flag = true; //더 이상 반복문을 실행할 필요가 없는지 확인 (갱신 확인)
-        for (int j = 0; j < m; j++) {
-            //s->d인 간선의 가중치가 w
+        bool flag = true;
+        for (int j = 0; j < edges.size(); j++) {
             int s = get<0>(edges[j]);
             int d = get<1>(edges[j]);
             int w = get<2>(edges[j]);
 
-            if (dist[s] == INF) { //아직 시작점에서 s로 가는 경로가 발견되지 않았으므로 갱신할 수 없음
+            if (dist[s] == INF) {
                 continue;
             }
-            ll weight = dist[s] + w;
+            int weight = dist[s] + w;
             if (weight < dist[d]) {
                 dist[d] = weight;
                 flag = false;
-                if (i == n) { //n번째 갱신이었다면 -> 음의 사이클
-                    return {-1};
+                if (i == n) {
+                    return {INF + 1}; //음의 사이클이 생겼음을 INF+1로 나타냄
                 }
             }
         }
-        if (flag) { //더 이상 갱신되지 않았다면 더 탐색할 필요 없음
+        if (flag) {
             break;
         }
     }
@@ -43,7 +39,14 @@ vector<ll> bellmanFord(int start, int n, int m, vector<tp> &edges) {
 /**
  * [웜홀]
  *
+ * 벨만-포드 문제
  *
+ * 시간이 되돌아가서 출발 지점에 도착하는 경우 = 음의 사이클이 생긴 경우
+ *
+ * 특별히 시작점이 주어지지 않았으므로, 음의 사이클이 생긴 경우를 모든 점을 시작점으로 고려해서 판단해야 함
+ * 이때, 모든 정점마다 벨만-포드를 돌리면 원래라면 시간초과라 "임의의 가짜 정점"을 만들어서 구함!
+ * => 가짜 정점 0을 만들어서 모든 정점과 가중치 0으로 연결시킨 후, 가짜 정점 0에서 벨만-포드 돌림
+ * 이렇게 되면 모든 정점을 시작 정점으로 고려하게 됨!
  */
 
 int main() {
@@ -53,26 +56,31 @@ int main() {
     cin >> tc;
     while (tc--) {
         cin >> n >> m >> w;
-        vector<tp> edges;
-        int edge_cnt = m * 2 + w;
-        while (m--) {
+        vector<tp> edges; //간선의 수
+        while (m--) { //도로 -> 무방향
             cin >> s >> e >> t;
             edges.push_back({s, e, t});
             edges.push_back({e, s, t});
         }
-        while (w--) {
+        while (w--) { //웜홀 -> 방향
             cin >> s >> e >> t;
             edges.push_back({s, e, -t});
         }
-        string ans = "NO";
+
+        //가짜 정점 생성
         for (int i = 1; i <= n; i++) {
-            vector<ll> dist = bellmanFord(i, n, edge_cnt, edges);
-            if (dist[0] == -1) {
-                ans = "YES";
-                break;
-            }
+            edges.push_back({0, i, 0});
         }
-        cout << ans << '\n';
+
+        //연산
+        vector<int> dist = bellmanFord(0, n + 1, edges);
+
+        //출력
+        if (dist[0] == INF + 1) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
     }
     return 0;
 }
