73a078b097763e3facffc3c91bbb71211844cc0c
jiyoung <jiyoung_06@ewhain.net>
2022-06-21 01:49:26 +0900

    [위상 정렬] 06월 21일

--- 06월 07일 - 위상 정렬/라이브 코딩/과제/2637.py
+++ 06월 07일 - 위상 정렬/라이브 코딩/과제/2637.py
@@ -6,10 +6,11 @@ input = sys.stdin.readline
 [장난감 부품]
 
 - 완제품을 만들기 위해 필요한 기본 부품의 개수를 구하는 문제
--> 위상 정렬을 역으로 수행하여 리프노드의 필요한 개수를 기록
+- x(만들어지는 부품) <- y(필요한 부품) 의 관계를 역으로 만들면 x -> y가 되면서 루트노드가 완제품이 되고 리프노드가 기본 부품이 됨
+- 따라서, BFS/DFS 탐색을 통해 필요한 리프노드의 총 개수를 구함
 """
 
-def reverse_topological(n, graph):
+def bfs(n, graph):
     que = deque()
     cnt = [0] * (n + 1)
     ans = [0] * (n + 1)
@@ -43,7 +44,7 @@ for _ in range(m):
     graph[x].append((y, k))
 
 # 연산
-ans = reverse_topological(n, graph)
+ans = bfs(n, graph)
 
 # 출력
 for i in range(1, n+1):
